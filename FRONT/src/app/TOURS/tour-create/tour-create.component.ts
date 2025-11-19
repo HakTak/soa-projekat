78f6a3596ae8067b25e895e026d2f3309c82f327
Aleksa Curaković <aleksa.curakovic@gmail.com>
2025-11-19 16:55:30 +0100

    downgrade angular

---
+++ FRONT/src/app/TOURS/tour-create/tour-create.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tour-create',
+  standalone: true,
+  imports: [],
+  templateUrl: './tour-create.component.html',
+  styleUrl: './tour-create.component.css'
+})
+export class TourCreateComponent {
+
+}
--- FRONT/src/app/app.routes.ts
+++ FRONT/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from '../home/home.component';
+import { TourCreateComponent } from './TOURS/tour-create/tour-create.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'tours', component: TourCreateComponent }
+];
