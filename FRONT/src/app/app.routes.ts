d931ba297593c2d5eb803918d4c3aac34fe0dd92
Aleksa Curaković <aleksa.curakovic@gmail.com>
2025-11-20 13:55:33 +0100

    dir fix

--- FRONT/src/app/app.routes.ts
+++ FRONT/src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from '../home/home.component';
-import { TourCreateComponent } from '../tour/tour-create/tour-create.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { TourCreateComponent } from './tour/tour-create/tour-create.component';
 import { BlogComponent } from './blog/blog.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: 'home', component: HomeComponent },
+    { path: 'home', component: HomepageComponent },
     { path: 'tours', component: TourCreateComponent },
     { path: 'blog', component: BlogComponent }
 ];
