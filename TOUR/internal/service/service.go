12c7d57df324771be8998689fea4b87e13a2aef4
HakTak <katek.m536@gmail.com>
2025-11-27 20:51:00 +0100

    Repo, model and service for toureecution

--- TOUR/internal/service/service.go
+++ TOUR/internal/service/service.go
@@ -1,17 +1,20 @@
 package service
 
 import (
+	"errors"
+	"time"
 	"tour-service/internal/model"
 	"tour-service/internal/repository"
 )
 
 type TourService struct {
-	repo       *repository.TourRepository
-	reviewRepo *repository.ReviewRepository
+	repo              *repository.TourRepository
+	reviewRepo        *repository.ReviewRepository
+	tourExecutionRepo *repository.TourExecutionRepository
 }
 
-func NewTourService(repo *repository.TourRepository, reviewRepo *repository.ReviewRepository) *TourService {
-	return &TourService{repo: repo, reviewRepo: reviewRepo}
+func NewTourService(repo *repository.TourRepository, reviewRepo *repository.ReviewRepository, tourExecutionRepo *repository.TourExecutionRepository) *TourService {
+	return &TourService{repo: repo, reviewRepo: reviewRepo, tourExecutionRepo: tourExecutionRepo}
 }
 
 func (s *TourService) CreateTour(t *model.Tour) error {
@@ -53,4 +56,80 @@ func (s *TourService) DeleteReview(id string) error {
 
 func (s *TourService) GetAllReviews() ([]model.Review, error) {
 	return s.reviewRepo.GetAllReviews()
+}
+
+func (s *TourService) CreateTourExecution(teDto model.TourCreateExecutionDTO) (*model.TourExecution, error) {
+	te := &model.TourExecution{
+		TourId:          teDto.TourId,
+		TouristId:       teDto.TouristId,
+		Status:          "ACTIVE",
+		StartedAt:       time.Now(),
+		CurrentPosition: teDto.CurrentPosition,
+	}
+
+	tour, err1 := s.repo.GetTour(teDto.TourId)
+
+	if err1 != nil {
+		return nil, err1
+	}
+
+	if tour.Status == "DRAFT" {
+		return nil, errors.New("Tura ne moze biti pokrenuta jer je u DRAFT statusu")
+	}
+
+	//ovde proveri da li je tura kupljena
+
+	err := s.tourExecutionRepo.Create(te)
+	return te, err
+}
+
+func (s *TourService) GetById(id string) (*model.TourExecution, error) {
+	return s.tourExecutionRepo.GetById(id)
+}
+
+func (s *TourService) FinishOrAbandon(id, status string) (*model.TourExecution, error) {
+	return s.tourExecutionRepo.FinishOrAbandon(id, model.TourExecutionStatus(status))
+}
+
+func (s *TourService) SetCurrentLocation(touristId string, curLoc model.TouristPosition) (int, error) {
+	return s.tourExecutionRepo.SetCurrentLocation(touristId, curLoc)
+}
+
+func (s *TourService) CheckCloseKeyPoints(id string, tourId string) (*model.TourExecution, error) {
+	kps, err1 := s.tourExecutionRepo.GetKeypointsByTour(tourId)
+
+	if err1 != nil {
+		return nil, err1
+	}
+
+	te, err2 := s.tourExecutionRepo.GetById(id)
+
+	if err2 != nil {
+		return nil, err2
+	}
+
+	kpId := model.FindNearbyKeypointId(kps, te.CurrentPosition.Latitude, te.CurrentPosition.Longitude)
+
+	if kpId == "" {
+		return nil, nil
+	}
+
+	completedKps := &model.CompletedKeypoint{
+		KeyPointId:  kpId,
+		CompletedAt: time.Now(),
+	}
+
+	te.CompledetKeyPonts = append(te.CompledetKeyPonts, *completedKps)
+
+	updatedTe, err3 := s.tourExecutionRepo.CompleteKeyPoint(id, te.CompledetKeyPonts)
+
+	if err3 != nil {
+		return nil, err3
+	}
+
+	if len(updatedTe.CompledetKeyPonts) < len(kps) {
+		return updatedTe, nil
+	}
+
+	return s.FinishOrAbandon(id, "COMPLETED")
 }
