7f0f9d3a5a180030e36cce6d2fcd83da9ad2d52e
Lakic <nikolalakic2@gmail.com>
2025-11-27 16:36:47 +0100

    pracenje logova

---
+++ API_GATEWAY/middleware/tracer.go
@@ -0,0 +1,61 @@
+package middleware
+
+import (
+	"log"
+	"os"
+
+	"go.opentelemetry.io/otel/exporters/jaeger"
+	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
+	"go.opentelemetry.io/otel/sdk/resource"
+	"go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
+)
+
+var tp *trace.TracerProvider
+
+const serviceName = "api-gateway"
+
+func InitTracer() (*trace.TracerProvider, error) {
+	// Ukoliko je definisana JAEGER_ENDPOINT env var, intanciraj JagerTracer koji šalje trace-ove Jaeger-u,
+	// u suprotnom instanciraj FileTracer koji upisuje trace-ove u json fajl
+	url := os.Getenv("JAEGER_ENDPOINT")
+	if len(url) > 0 {
+		return InitJaegerTracer(url)
+	} else {
+		return InitFileTracer()
+	}
+}
+
+func InitFileTracer() (*trace.TracerProvider, error) {
+	log.Println("Initializing tracing to traces.json")
+	f, err := os.Create("traces.json")
+	if err != nil {
+		return nil, err
+	}
+	exporter, err := stdouttrace.New(
+		stdouttrace.WithWriter(f),
+		stdouttrace.WithPrettyPrint(),
+	)
+	if err != nil {
+		return nil, err
+	}
+	return trace.NewTracerProvider(
+		trace.WithBatcher(exporter),
+		trace.WithSampler(trace.AlwaysSample()),
+	), nil
+}
+
+func InitJaegerTracer(url string) (*trace.TracerProvider, error) {
+	log.Printf("Initializing tracing to jaeger at %s\n", url)
+	exporter, err := jaeger.New(jaeger.WithCollectorEndpoint(jaeger.WithEndpoint(url)))
+	if err != nil {
+		return nil, err
+	}
+	return trace.NewTracerProvider(
+		trace.WithBatcher(exporter),
+		trace.WithResource(resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceNameKey.String(serviceName),
+		)),
+	), nil
+}
